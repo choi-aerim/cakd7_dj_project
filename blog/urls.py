8ed46771c97ed4d604bf50257c7278179d746b96
choi_aerim <kqy951118@gmail.com>
2022-08-08 15:17:51 +0900

    이미지, 파일 기능 추가

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from xml.etree.ElementInclude import include
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
--- dj/settings.py
+++ dj/settings.py
@@ -120,3 +120,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
