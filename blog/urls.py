940ce14d9febd103c99029cd9fd1d761c69f3532
choi_aerim <kqy951118@gmail.com>
2022-08-17 14:00:08 +0900

    댓글 수정 기능 추가

--- blog/urls.py
+++ blog/urls.py
@@ -19,10 +19,13 @@ urlpatterns = [
     # 새 포스트 생성시, creat_post 클래스로 보냄
     path('create_post/', views.PostCreate.as_view()),
 
-    #만들어진 포스트 수정시, 
+    # 포스트 수정
     path('update_post/<int:pk>/', views.PostUpdate.as_view()),
 
     # 댓글
     path('<int:pk>/new_comment/', views.new_comment),
+
+    # 댓글 수정
+    path('update_comment/<int:pk>/', views.CommentUpdate.as_view()),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
+from xml.etree.ElementTree import Comment
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 from .forms import CommentForm
 from django.core.exceptions import PermissionDenied
 from django.utils.text import slugify
@@ -201,6 +202,16 @@ def new_comment(request,pk):
     else:
         raise PermissionDenied
 
+# 댓글 수정
+class CommentUpdate(LoginRequiredMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(CommentUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
 
 
     
